d30c0e555d28e85d649ac4929a3f9068f2451d71
OhtoAi <zhuguangfeng.thatboy@gmail.com>
2022-01-27 15:00:38 +0800

    Feature Impl
    Signed-off-by: OhtoAi <zhuguangfeng.thatboy@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,55 @@
 #include <fplus/fplus.hpp>
 #include <iostream>
 
-constexpr int ServerPort = 8080;
+auto applicationConfig =
+    nlohmann::json::parse(R"(
+    {
+        "folder-path":".",
+        "server":
+        {
+            "host":"localhost",
+            "port":8080
+        }
+    }
+)");
 
+void loadConfig();
 void HandleFile(const httplib::Request &, httplib::Response &);
 
 int main(int argc, char const *argv[])
 {
-    httplib::Server server;
     spdlog::set_level(spdlog::level::debug);
-
     spdlog::info("Hello MFV!");
 
-    spdlog::info("HTTP Server will listen on {}:{}.", "localhost", ServerPort);
-    if (server.bind_to_port("localhost", ServerPort))
+    loadConfig();
+
+    if (argc == 2)
+    {
+        spdlog::info("Use arg = {}.", argv[argc - 1]);
+        applicationConfig["server"]["port"] = std::atoi(argv[argc - 1]);
+    }
+    else if (argc == 3)
+    {
+        spdlog::info("Use arg = {} {}.", argv[argc - 2], argv[argc - 1]);
+        applicationConfig["server"]["host"] = argv[argc - 2];
+        applicationConfig["server"]["port"] = std::atoi(argv[argc - 1]);
+    }
+
+    httplib::Server server;
+
+    spdlog::info("HTTP Server will listen on {}:{}.", applicationConfig["server"]["host"], applicationConfig["server"]["port"].get<int>());
+    if (server.bind_to_port(applicationConfig["server"]["host"].get<std::string>().c_str(), applicationConfig["server"]["port"].get<int>()))
     {
         spdlog::info("Server bind Done.");
     }
     else
     {
-        spdlog::critical("Server bind Failed. Cannot bind to port:{}", ServerPort);
+        spdlog::critical("Server bind Failed. Cannot bind to {}:{}", applicationConfig["server"]["host"], applicationConfig["server"]["port"]);
         return -1;
     }
 
-    server.Get("/.*", HandleFile);
+    server.set_mount_point("/", "html");
+    server.Get("/file:(.*)", HandleFile);
 
     if (server.listen_after_bind())
     {
@@ -42,24 +68,115 @@ int main(int argc, char const *argv[])
 
 void HandleFile(const httplib::Request &req, httplib::Response &res)
 {
-    spdlog::info("{} {} from {:<30}:{}", req.method, req.path, req.remote_addr, req.remote_port);
+    nlohmann::json j;
+    ghc::filesystem::path requirePath(req.matches[1]);
 
-    auto firstAccept = fplus::split(',', false, req.get_header_value("accept")).front();
-    spdlog::debug("First accept: {}", firstAccept);
+    j["require_path"] = requirePath.string();
 
-    if(firstAccept == "text/html")
+    try
     {
-        res.set_content("<h1>test render page</h1>", firstAccept.c_str());
+        do
+        {
+            requirePath = applicationConfig["folder-path"].get<std::string>() + "/" + requirePath.string();
+            j["real_path"] = requirePath.string();
+            spdlog::debug("visit real path = {}", j["real_path"]);
+            spdlog::debug("visit absolute path = {}", ghc::filesystem::absolute(j["real_path"]).string());
+
+            if (!ghc::filesystem::exists(requirePath))
+            {
+                res.status = 404;
+                j["code"] = 404;
+                j["success"] = true;
+                j["message"] = httplib::detail::status_message(j["code"]);
+                break;
+            }
+            SPDLOG_DEBUG("test");
+            if (ghc::filesystem::is_directory(requirePath))
+            {
+                j["folder"] = true;
+
+                for (const auto &item : ghc::filesystem::directory_iterator(requirePath))
+                {
+                    nlohmann::json i;
+                    i["name"] = item.path().filename().string();
+                    i["path"] = item.path().string();
+                    i["absolute_path"] = ghc::filesystem::absolute(item.path()).string();
+                    i["url"] = fmt::format("http:{}:{}/blob:{}", applicationConfig["server"]["host"], applicationConfig["server"]["port"].get<int>(), item.path().string());
+                    i["attrib"]["directory"] = item.is_directory();
+                    i["attrib"]["character_file"] = item.is_character_file();
+                    i["attrib"]["block_file"] = item.is_block_file();
+                    i["attrib"]["other"] = item.is_other();
+                    i["attrib"]["symlink"] = item.is_symlink();
+                    i["attrib"]["regular_file"] = item.is_regular_file();
+                    i["attrib"]["socket"] = item.is_socket();
+                    i["attrib"]["fifo"] = item.is_fifo();
+
+                    j["content"].push_back(i);
+
+                    spdlog::debug("directory visit path = {}", item.path().string());
+                }
+            }
+            else
+            {
+                j["folder"] = false;
+            }
+
+            res.status = 200;
+            j["code"] = 200;
+            j["success"] = true;
+            j["message"] = httplib::detail::status_message(j["code"]);
+
+        } while (false);
     }
-    else if(firstAccept == "application/json")
+    catch (nlohmann::json::invalid_iterator e)
     {
-        nlohmann::json j;
-        j["status"] = "ok";
-        j["path"] = "/";
-        res.set_content(j.dump(4), firstAccept.c_str());
+        spdlog::error("nlohmann::json::invalid_iterator occur @HandleFile {}", e.what());
+    }
+    catch (nlohmann::json::parse_error e)
+    {
+        spdlog::error("nlohmann::json::parse_error occur @HandleFile: {}", e.what());
+    }
+    catch (nlohmann::json::type_error e)
+    {
+        spdlog::error("nlohmann::json::type_error occur @HandleFile {}", e.what());
+    }
+    catch (nlohmann::json::out_of_range e)
+    {
+        spdlog::error("nlohmann::json::out_of_range occur @HandleFile {}", e.what());
+    }
+    catch (...)
+    {
+        spdlog::error("Unknow error occur @HandleFile");
+    }
+
+    res.set_content(j.dump(4), "application/json");
+    spdlog::info("{} {} {} \t\tfrom {}:{}", j["code"].get<int>(), req.method, req.path, req.remote_addr, req.remote_port);
+}
+
+void loadConfig()
+{
+    ghc::filesystem::path applicationConfigPath{"application.json"};
+
+    if (ghc::filesystem::exists(applicationConfigPath))
+    {
+        spdlog::info("Load config from {}", applicationConfigPath.string());
+        try
+        {
+            ghc::filesystem::ifstream applicationConfigFile(applicationConfigPath);
+            applicationConfig.merge_patch(nlohmann::json::parse(std::string{std::istreambuf_iterator<char>(applicationConfigFile),
+                                                                            std::istreambuf_iterator<char>()}));
+        }
+        catch (...)
+        {
+            spdlog::critical("Unable to load {}, check the content and try again!", applicationConfigPath.string());
+            throw;
+        }
     }
     else
     {
-        res.status = 403;
+        spdlog::info("Generate config {}", applicationConfigPath.string());
+        ghc::filesystem::ofstream applicationConfigFile(applicationConfigPath);
+        applicationConfigFile << applicationConfig.dump(4);
+        applicationConfigFile.close();
     }
 }
